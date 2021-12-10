ade5ae5f6d20d9649addc9bc394e6dc963550b78
Gloria Lee <glorialee820@gmail.com>
2021-12-10 10:08:34 -0500

    New changes by HPC team for Pull Request

--- marketplace.tf
+++ marketplace.tf
@@ -1,72 +1,46 @@
-## Copyright (c) 2020, Oracle and/or its affiliates. 
-## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
-
-resource "oci_marketplace_accepted_agreement" "Accepted_Agreement" {
-  agreement_id    = oci_marketplace_listing_package_agreement.Listing_Package_Agreement.agreement_id
-  compartment_id = var.compartment_ocid
-  listing_id      = data.oci_marketplace_listing.Listing.id
-  package_version = data.oci_marketplace_listing.Listing.default_package_version
-  signature       = oci_marketplace_listing_package_agreement.Listing_Package_Agreement.signature
+locals {
+//  listing_number = split(".", var.marketplace_listing)[0]
+    mp_listing_id = var.use_marketplace_image ? var.marketplace_listing_id : ""
+    mp_version_id = split(".", var.marketplace_listing)[0]
 }
 
-resource "oci_marketplace_listing_package_agreement" "Listing_Package_Agreement" {
-  agreement_id    = data.oci_marketplace_listing_package_agreements.Listing_Package_Agreements.agreements.0.id
-  listing_id      = data.oci_marketplace_listing.Listing.id
-  package_version = data.oci_marketplace_listing.Listing.default_package_version
+/* 
+output "debug" {
+  value = data.oci_core_app_catalog_listing_resource_versions.app_catalog_listing_resource_versions.app_catalog_listing_resource_versions[0].listing_resource_id
+}
+*/ 
+
+/*
+data "oci_core_app_catalog_listing" "app_catalog_listing" {
+    count = var.use_marketplace_image ? 1 : 0
+    listing_id = local.mp_listing_id
+}
+*/ 
+
+data "oci_core_app_catalog_listing_resource_versions" "app_catalog_listing_resource_versions" {
+    count = var.use_marketplace_image ? 1 : 0
+    listing_id = local.mp_listing_id
 }
 
-data "oci_marketplace_listing_package_agreements" "Listing_Package_Agreements" {
-  listing_id      = data.oci_marketplace_listing.Listing.id
-  package_version = data.oci_marketplace_listing.Listing.default_package_version
-  compartment_id = var.compartment_ocid
+resource "oci_core_app_catalog_listing_resource_version_agreement" "mp_image_agreement" {
+  count = var.use_marketplace_image ? 1 : 0
+
+  listing_id               = local.mp_listing_id
+  listing_resource_version = var.marketplace_version_id[local.mp_version_id]
+
 }
 
-data "oci_marketplace_listing_package" "Listing_Package" {
-  listing_id      = data.oci_marketplace_listing.Listing.id
-  package_version = data.oci_marketplace_listing.Listing.default_package_version
-  compartment_id = var.compartment_ocid
-}
+resource "oci_core_app_catalog_subscription" "mp_image_subscription" {
+  count                    = var.use_marketplace_image ? 1 : 0
+  compartment_id           = var.targetCompartment
+  eula_link                = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement[0].eula_link
+  listing_id               = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement[0].listing_id
+  listing_resource_version = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement[0].listing_resource_version
+  oracle_terms_of_use_link = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement[0].oracle_terms_of_use_link
+  signature                = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement[0].signature
+  time_retrieved           = oci_core_app_catalog_listing_resource_version_agreement.mp_image_agreement[0].time_retrieved
 
-data "oci_marketplace_listing_packages" "Listing_Packages" {
-  listing_id      = data.oci_marketplace_listing.Listing.id
-  compartment_id = var.compartment_ocid
-}
-
-data "oci_marketplace_listing" "Listing" {
-  listing_id      = data.oci_marketplace_listings.Listings.listings.0.id
-  compartment_id = var.compartment_ocid
-}
-
-data "oci_marketplace_listings" "Listings" {
-  pricing        = ["Free"]
-  name           = ["NVIDIA GPU Cloud Machine Image"]
-  compartment_id = var.compartment_ocid
-}
-
-data "oci_core_app_catalog_listing" "App_Catalog_Listing" {
-    listing_id = data.oci_marketplace_listing_package.Listing_Package.app_catalog_listing_id
-}
-
-data "oci_core_app_catalog_listing_resource_versions" "App_Catalog_Listing_Resource_Versions" {
-    listing_id = data.oci_marketplace_listing_package.Listing_Package.app_catalog_listing_id
-}
-
-data "oci_core_app_catalog_listing_resource_version" "App_Catalog_Listing_Resource_Version" {
-    listing_id       = data.oci_marketplace_listing_package.Listing_Package.app_catalog_listing_id
-    resource_version = data.oci_core_app_catalog_listing_resource_versions.App_Catalog_Listing_Resource_Versions.app_catalog_listing_resource_versions[0].listing_resource_version
-}
-
-resource "oci_core_app_catalog_listing_resource_version_agreement" "App_Catalog_Listing_Resource_Version_Agreement" {
-    listing_id               = data.oci_marketplace_listing_package.Listing_Package.app_catalog_listing_id
-    listing_resource_version = data.oci_core_app_catalog_listing_resource_versions.App_Catalog_Listing_Resource_Versions.app_catalog_listing_resource_versions[0].listing_resource_version
-}
-
-resource "oci_core_app_catalog_subscription" "App_Catalog_Subscription" {
-    compartment_id           = var.compartment_ocid
-    eula_link                = oci_core_app_catalog_listing_resource_version_agreement.App_Catalog_Listing_Resource_Version_Agreement.eula_link
-    listing_id               = oci_core_app_catalog_listing_resource_version_agreement.App_Catalog_Listing_Resource_Version_Agreement.listing_id
-    listing_resource_version = oci_core_app_catalog_listing_resource_version_agreement.App_Catalog_Listing_Resource_Version_Agreement.listing_resource_version
-    oracle_terms_of_use_link = oci_core_app_catalog_listing_resource_version_agreement.App_Catalog_Listing_Resource_Version_Agreement.oracle_terms_of_use_link
-    signature                = oci_core_app_catalog_listing_resource_version_agreement.App_Catalog_Listing_Resource_Version_Agreement.signature
-    time_retrieved           = oci_core_app_catalog_listing_resource_version_agreement.App_Catalog_Listing_Resource_Version_Agreement.time_retrieved
+  timeouts {
+    create = "20m"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,71 +1,129 @@
-## Copyright © 2020, Oracle and/or its affiliates. 
-## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
-
 variable "region" {}
 variable "tenancy_ocid" {}
-variable "compartment_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "availablity_domain_name" {}
-
-variable "release" {
-  description = "Reference Architecture Release (OCI Architecture Center)"
-  default     = "1.0"
+variable "targetCompartment" {}
+variable "ad" {}
+variable "ssh_key" {}
+variable "cluster_network" { default = true }
+variable "use_custom_name" {}
+variable "cluster_name" { default = "" }
+variable "bastion_ad" {}
+variable "bastion_shape" {}
+variable "use_standard_image" {}
+variable "custom_bastion_image" {
+  type    = string
+  default = "image.ocid"
+}
+variable "bastion_boot_volume_size" {}
+variable "cluster_network_shape" { default = "BM.HPC2.36" }
+variable "instance_pool_shape" { default = "VM.Standard2.4" }
+variable "node_count" {}
+variable "boot_volume_size" {}
+variable "use_marketplace_image" {}
+variable "image" { default = "ocid1.image.oc1..aaaaaaaa5yxem7wzie34hi5km4qm2t754tsfxrjuefyjivebrxjad4jcj5oa" }
+variable "image_ocid" { default = "ocid1.image.oc1..aaaaaaaa5yxem7wzie34hi5km4qm2t754tsfxrjuefyjivebrxjad4jcj5oa" }
+variable "unsupported_bastion_image" { default = "" }
+variable "use_cluster_nfs" { default = true }
+variable "use_scratch_nfs" { default = true }
+variable "cluster_nfs_path" { default = "/nfs/cluster" }
+variable "scratch_nfs_path" { default = "/nfs/scratch" }
+variable "vcn_compartment" { default = "" }
+variable "vcn_id" { default = "" }
+variable "use_existing_vcn" {}
+variable "public_subnet_id" { default = "" }
+variable "private_subnet_id" { default = "" }
+variable "vcn_subnet" { default = "" }
+variable "public_subnet" { default = "" }
+variable "additional_subnet" { default = "" }
+variable "private_subnet" { default = "" }
+variable "ssh_cidr" { default = "0.0.0.0/0" }
+#namd
+variable "namd" { default = true }
+variable "namd_binaries" {}
+variable "namd_model" {}
+variable "slurm" { default = false }
+variable "ldap" { default = true }
+variable "spack" { default = false }
+variable "bastion_ocpus" { default = 2 }
+variable "instance_pool_ocpus" { default = 2 }
+variable "instance_pool_memory" { default = 16 }
+variable "instance_pool_custom_memory" { default = false }
+variable "bastion_memory" { default = 16 }
+variable "bastion_custom_memory" { default = false }
+variable "marketplace_listing" {
+  default = "1. OL7.9-Gen2-GPU-2021.10.20-0"
 }
 
-variable "VCN-CIDR" {
-  default = "10.0.0.0/16"
+variable "marketplace_version_id" {
+  type = map(string)
+  default = {
+    "1" = "OL7.9-Gen2-GPU-2021.10.20-0"
+  }
 }
 
-variable "Subnet-CIDR" {
-  default = "10.0.0.0/24"
+variable "marketplace_listing_id" {
+  default = "ocid1.appcataloglisting.oc1..aaaaaaaahzcnanlki5vonyaeoiajjisejikzczygqqwheifymjqx3ft4iowa"
 }
 
-variable "gpu_node_count" {
-    default = "1"
-}
-variable "vnc_password" {
-    default = "HPC_oci1"
-}
+variable "bastion_block_volume_performance" {
+  /* 
+  Allowed values 
+  "0.  Lower performance"
+  "10. Balanced performance"
+  "20. High Performance"
+*/
 
-variable "gpu_shape" { 
-    default = "BM.GPU2.2"
-}
+  default = "10. Balanced performance"
 
-# Possible values are none, vnc or x11vnc
-# If x11vnc is selected and NVIDIA drivers are not available, vnc will be used.
-variable "gpu_vnc" { 
-    default = "vnc"
 }
 
-# Install Block NFS, value are 0 or 1
-variable "block_nfs" {
-  default = "True"
+variable "bastion_block" {
+  default = false
 }
 
-# Size in GB
-variable "size_block_volume" {
-  default = "500"
+variable "bastion_block_volume_size" {
+  default = 1000
+}
+variable "scratch_nfs_type_cluster" { default = "nvme" }
+variable "scratch_nfs_type_pool" { default = "none" }
+variable "cluster_block_volume_size" { default = "1000" }
+variable "cluster_block_volume_performance" { default = "10. Balanced performance" }
+
+variable "inst_prin" { default = true }
+variable "api_user_key" { default = "None" }
+variable "api_fingerprint" { default = "None" }
+variable "api_user_ocid" { default = "None" }
+variable "home_nfs" { default = true }
+variable "configure" { default = true }
+
+variable "hyperthreading" { default = true }
+
+variable "autoscaling" { default = false }
+variable "add_nfs" { default = false }
+variable "create_ffs" { default = false }
+variable "fss_compartment" { default = "" }
+variable "fss_ad" { default = "" }
+variable "nfs_target_path" { default = "/app" }
+variable "nfs_source_IP" { default = "" }
+variable "nfs_source_path" { default = "/app" }
+variable "nfs_options" { default = "" }
+variable "monitoring" { default = true }
+
+variable "unsupported" {
+  type    = bool
+  default = false
 }
 
-variable "model_drive" {
-  default = "block"
+variable "unsupported_bastion" {
+  type    = bool
+  default = false
 }
 
-variable "devicePath" {
-  default = "/dev/oracleoci/oraclevdb"
+variable "bastion_username" {
+  type    = string
+  default = "opc"
 }
 
-
-variable "namd_url" { 
-    default = "https://objectstorage.us-phoenix-1.oraclecloud.com/p/3YWzx8pwApyEYHigvRLFdLNlTlOwzp1hBeXwompHxfI/n/hpc/b/HPC_APPS/o/NAMD_2.13_Linux-x86_64-multicore-CUDA.tar"
-}
-
-variable "model_url" { 
-    default = "https://objectstorage.us-phoenix-1.oraclecloud.com/p/dtQofvnX4K4BOHfVGuq8oCdtJFt4lP0UXkod1BIQBYk/n/hpc/b/HPC_APPS/o/NAMD_models.tar"
-}
-
-variable "vmd_url" { 
-    default = "https://objectstorage.us-phoenix-1.oraclecloud.com/p/bpR0VM0GaNlc4yQsGLwO4lZwneKxXwN8vUdtyKcVZcE/n/hpc/b/HPC_APPS/o/vmd-1.9.3.bin.LINUXAMD64-CUDA8-OptiX4-OSPRay111p1.opengl.tar.gz"
+variable "compute_username" {
+  type    = string
+  default = "opc"
 }
